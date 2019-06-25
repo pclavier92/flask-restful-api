87e17547f268645c9faff647c932626ff70e64c9
Philippe Clavier <pclavier92@gmail.com>
2019-06-25 13:20:44 -0300

    Fix creating db automatically localy

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
+
 jwt = JWT(app, authenticate, identity) # /auth
 
 api.add_resource(UserRegister,'/register')
